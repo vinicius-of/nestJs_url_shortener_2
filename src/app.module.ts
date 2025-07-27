bbd055ca037b692e74e3a091f89bf520a0702e0b
Vinicius Fonseca <vinicius.oliveirafonseca@gmail.com>
2025-07-27 15:25:06 -0300

    feat(app): Basic app root files

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { UrlsModule } from './urls/urls.module';
+import { AuthModule } from './auth/auth.module';
+import { UrlsModule } from './urls/urls.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [UsersModule, AuthModule, UrlsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
